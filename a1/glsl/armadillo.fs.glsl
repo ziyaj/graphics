57f4a452463cff8dbfa676989cf31bd49a184644
Ziyang Jin <ziyang.jin.master@gmail.com>
2018-01-11 18:08:12 -0800

    Change fragment shader to produce a grey-shaded Armadillo

--- a1/glsl/armadillo.fs.glsl
+++ a1/glsl/armadillo.fs.glsl
@@ -4,7 +4,9 @@ varying vec3 interpolatedNormal;
 void main() {
     // Set final rendered color according to the surface normal
   vec3 N = normalize(interpolatedNormal);
-  gl_FragColor = vec4(N, 1.0);
+  vec3 L = vec3(0.0, 0.0, -1.0);
+  float i = dot(N, L);
+  gl_FragColor = vec4(i, i, i, 1.0);
   // shade all pixels of the Armadillo green
   // gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }
